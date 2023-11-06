9ab0c8bcd7cf70daf7a164e83e6f22dc7af37046
GallardoDev <115595877+GallardoDev@users.noreply.github.com>
2023-11-06 07:44:24 -0800

    Actualizar settings.py
    Cambiando email por example

--- Online_Store/settings.py
+++ Online_Store/settings.py
@@ -147,9 +147,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gallardosapp@gmail.com'
+EMAIL_HOST_USER = 'example@gmail.com'
 EMAIL_HOST_PASSWORD = 'pwis dshc hmfu prjq'
-DEFAULT_FROM_EMAIL = 'gallardosapp@gmail.com'
+DEFAULT_FROM_EMAIL = 'example@gmail.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
